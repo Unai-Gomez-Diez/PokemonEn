db91110d3c0a69eb43792ff5bb7516a864989c1b
Enrique <arenasvinasenrique@gmail.com>
2024-10-19 23:39:55 +0200

    Creacion de la clase data en pokemon

---
+++ app/src/main/java/edu/iesam/pokemon/data/local/PokemonXmlLocalDataSource.kt
@@ -0,0 +1,49 @@
+package edu.iesam.pokemon.data.local
+
+import android.content.Context
+import com.google.gson.Gson
+import edu.iesam.pokemon.domain.Pokemon
+
+class PokemonXmlLocalDataSource(private val context: Context) {
+
+    private val sharedPref = context.getSharedPreferences(
+        "pokemon", Context.MODE_PRIVATE
+    )
+
+    private val gson = Gson()
+
+    fun save(pokemon: Pokemon) {
+        val editor = sharedPref.edit()
+        editor.putString(pokemon.name, gson.toJson(pokemon))
+        editor.apply()
+    }
+
+    fun delete(){
+        val editor = sharedPref.edit()
+        editor.clear()
+        editor.apply()
+    }
+
+    fun saveAll(pokemons: List<Pokemon>) {
+        val editor = sharedPref.edit()
+        pokemons.forEach {
+            editor.putString(it.name, gson.toJson(it))
+        }
+        editor.apply()
+    }
+
+    fun findAll(): List<Pokemon> {
+        val pokemons = ArrayList<Pokemon>()
+        val mapPokemon = sharedPref.all
+        mapPokemon.forEach {
+            pokemons.add(gson.fromJson(it.value as String, Pokemon::class.java))
+        }
+        return pokemons
+    }
+
+    fun deleteById(id: String) {
+        val editor = sharedPref.edit()
+        editor.remove(id)
+        editor.apply()
+    }
+}
--- app/src/main/java/edu/iesam/pokemon/domain/Pokemon.kt
+++ app/src/main/java/edu/iesam/pokemon/domain/Pokemon.kt
@@ -1,8 +1,14 @@
 package edu.iesam.pokemon.domain
 
 data class Pokemon(
-    val id: Int,
     val name: String,
-    val types: List<String>,
-    val imageUrl: String
+    val id: Int,
+    val height: Int,
+    val weight: Int,
+    val sprites: Sprites
 )
+
+data class Sprites(
+    val front_default: String
+)
+
